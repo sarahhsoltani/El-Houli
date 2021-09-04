d009d08f72172e2f417ed2a776df42d6594abd7d
sarahhsoltani <sarasoltani.simplon.enda@gmail.com>
2021-09-04 16:57:39 +0100

    add to cards

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -5,50 +5,50 @@ module.exports={
     // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
-addOrderItems : async (req, res) => {
-    const {
-      orderItems,
+addOrderItems: async (req, res) => {
+  try {
       
-    } = req.body
-  
-    if (orderItems && orderItems.length === 0) {
-      res.status(400)
-      throw new Error('No order items')
-      return
-    } else {
-      const order = new Order({
-        orderItems,
-         user: req.user._id    
-        
-      })
-  
-      const createdOrder = await order.save()
-  
-      res.status(201).json(createdOrder)
-    }
-  },
+    const { title,qty,image,price } = req.body;
+    
+    const orders = new Order({
+      title,
+      qty,
+      image,
+      price,
+     
+     user: req.user._id 
+    });
+    orders.save();
+    res.status(201).send(orders);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+},
+
   // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
- getOrderById:async (req, res) => {
-  const order = await Order.findById(req.params.id).populate(
-    'user',
-    'name email'
-  )
-  if (order) {
-    res.json(order)
-  } else {
-    res.status(404)
-    throw new Error('Order not found')
+
+getOrderById: async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id)
+      .populate("user", "-password")
+      
+
+    return res.send(order);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
   }
-},
+}
 
 // @desc    Get all orders
 // @route   GET /api/orders
 // @access  Private/Admin
- getOrders : async (req, res) => {
-  const orders = await Order.find({}).populate('user', 'id name')
-  res.json(orders)
-}
+//  getOrders : async (req, res) => {
+//   const orders = await Order.find({}).populate('user', 'id name')
+//   res.json(orders)
+// }
   
-}
+} 
--- Backend/routes/orderRouter.js
+++ Backend/routes/orderRouter.js
@@ -3,10 +3,10 @@ const router=express.Router()
 const order=require("../controllers/orderController")
 const authentification =require ("../middleware/auth")
 
-//post order
-router.post("/postOrder",authentification,order.addOrderItems)
+//post order 
+router.post("/postOrder/:id",authentification,order.addOrderItems)
 // get order byId
-router.get("/getOrderById/:id",order.getOrderById)
+router.get("/getOrderById/:id",authentification,order.getOrderById)
 // get All orders
-router.get("/getOrders",order.getOrders)
+//router.get("/getOrders",order.getOrders)
 module.exports=router  
